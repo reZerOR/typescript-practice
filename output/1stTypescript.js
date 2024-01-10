f46b1045d1d08824ee08786ac08782575d7e10c3
reZerOR <gkraihan14k@gmail.com>
2024-01-10 20:53:29 +0600

    function implicit data type

--- output/1stTypescript.js
+++ output/1stTypescript.js
@@ -5,9 +5,26 @@ another data type value typscript will give you error.
 */
 // let playername = 'raihan';
 // console.log(playername);
-// playername = 55;
+// playername = 55;  /* will give you an error*/
 // console.log(playername);
-let playername;
-playername = 'raihan';
-playername = 55;
-console.log(playername);
+/*Now this another scenario where you just declare the variable then typscript can't
+know what is data type of that variable then you can re assign again and again
+*/
+// let playername;
+// playername = 'raihan';
+// playername = 55 /*it will not give you an error */
+// console.log(playername)
+/* this function is working like vanila javascript because you didn't type the implicit types in
+function argument
+*/
+// function multiply (a, b){
+//     return a*b;
+// }
+// console.log(multiply(4, 7)) 
+// console.log(multiply('raihan', 8)) /* no error */
+/*  */
+function multiplyWithImplicitTypes(a, b) {
+    return a * b;
+}
+console.log(multiplyWithImplicitTypes(56, 78));
+console.log(multiplyWithImplicitTypes("raihan", 78));
