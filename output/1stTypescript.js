6516eba7c8043a5f6dd6cae1418bfc4d69b470e9
reZerOR <gkraihan14k@gmail.com>
2024-01-10 20:40:45 +0600

    learning how types work

--- output/1stTypescript.js
+++ output/1stTypescript.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('i love bangladesh');
+/* When you declare and assign a variable then typescript automaticly
+know that the variable's data type. If you want re assign the variable with
+another data type value typscript will give you error.
+*/
+// let playername = 'raihan';
+// console.log(playername);
+// playername = 55;
+// console.log(playername);
+let playername;
+playername = 'raihan';
+playername = 55;
+console.log(playername);
