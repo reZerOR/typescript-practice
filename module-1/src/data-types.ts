6f7a3f207471907d3cb11cb9f97cba99964a6448
reZerOR <gkraihan14k@gmail.com>
2024-05-02 08:55:04 +0600

    object, literal, optional, readonly values

--- module-1/src/data-types.ts
+++ module-1/src/data-types.ts
@@ -1,28 +1,42 @@
 // data types
 
-
 // string type
-let person_name : string = "who know"
+let person_name: string = "who know";
 
-// number type 
-let nums : number = 90
+// number type
+let nums: number = 90;
 
 // boolean type
-let isAdmin: boolean = true
+let isAdmin: boolean = true;
 
 // undifined
-let x: undefined = undefined
+let x: undefined = undefined;
 
 // null
-let y: null = null
-
+let y: null = null;
 
 // array
-let stringArray: string[] = ["name", "fame"]
+let stringArray: string[] = ["name", "fame"];
 
-let numList: number[] = [89, 69]
+let numList: number[] = [89, 69];
 
 // tuple --> array --> order --> type of values
-let coordinates: [number, number] = [67, 90]
+let coordinates: [number, number] = [67, 90];
 
-let ageName: [number, string] = [50, "name"]
+let ageName: [number, string] = [50, "name"];
+
+// reference type --> object
+const user: {
+  company: "turkeyIsp"; //type --> literal types
+  firstName: string;
+  lastName: string;
+  middleName?: string; //optional type
+  isMarried: boolean;
+  readonly phoneNumber: string; // read only type the value can not be changed
+} = {
+  company: "turkeyIsp",
+  firstName: "jorge",
+  lastName: "boosh",
+  isMarried: false,
+  phoneNumber: "+1234567890"
+};
